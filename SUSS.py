5e2da6c53c3989cd06f822b054660e3b5d1e5f17
DiegoCon06 <147194771+DiegoCon06@users.noreply.github.com>
2025-04-30 01:18:04 -0500

    Update SUSS.py

--- SUSS.py
+++ SUSS.py
@@ -18,7 +18,7 @@ def test_response_time(server_ip):
     response_times = []
     ping_cmd = ["ping", "-c", "3", server_ip]
     try:
-        for _ in range(3):  # Measure 3 response times
+        for _ in range(3): # Measure 3 response times
             start_time = time.time()
             process = subprocess.Popen(ping_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             process.communicate()
@@ -28,16 +28,15 @@ def test_response_time(server_ip):
             return sum(response_times) / len(response_times)
     except Exception as e:
         print("Unexpected error during ping:", e)
-        return None  # Server unreachable
-
+        return None # Server unreachable
 
 def generate_cpu_load(duration=60):
     """Generate CPU load by performing intensive calculations."""
     end_time = time.time() + duration
     while time.time() < end_time:
-        [x**2 for x in range(100000)]  # Increased range
-        for _ in range(100):  # Added nested loop
-            [x**3 for x in range(10000)]  # More complex calculations
+        [x**2 for x in range(100000)] # Increased range
+        for _ in range(100): # Added nested loop
+            [x**3 for x in range(10000)] # More complex calculations
 
 def write_report(report_file):
     """Write system report to the file."""
@@ -45,27 +44,33 @@ def write_report(report_file):
         with open(report_file, "w") as f:
             f.write("System Report - {}\n".format(datetime.now()))
             
-            # Generate CPU load for 60 seconds
-            print(psutil.cpu_percent(interval=60))
-            generate_cpu_load(duration=60)
-            print(psutil.cpu_percent(interval=60))
-            # Increase the interval to 15 seconds for CPU utilization measurement
-            cpu_utilization = psutil.cpu_percent(interval=15)
-            print("CPU Utilization: {}%".format(cpu_utilization))  # Debugging print statement
-            f.write("\nCPU Utilization: {}%\n".format(cpu_utilization))
-            f.write("^average CPU usage percentage over a 15-second interval\n")
+            # Generate CPU load for 60 seconds and measure CPU utilization continuously
+            cpu_utilizations = []
+            end_time = time.time() + 60
+            while time.time() < end_time:
+                cpu_utilizations.append(psutil.cpu_percent(interval=1))
+                [x**2 for x in range(100000)] # Increased range
+                for _ in range(100): # Added nested loop
+                    [x**3 for x in range(10000)] # More complex calculations
+            average_cpu_utilization = sum(cpu_utilizations) / len(cpu_utilizations)
+            print("Average CPU Utilization: {}%".format(average_cpu_utilization)) # Debugging print statement
+            f.write("\nAverage CPU Utilization: {}%\n".format(average_cpu_utilization))
+            f.write("^average CPU usage percentage during load generation\n")
+            
             try:
                 load_avg = psutil.getloadavg()[0]
-                print("User Load Average: {}".format(load_avg))  # Debugging print statement
+                print("User Load Average: {}".format(load_avg)) # Debugging print statement
                 f.write("\nUser Load Average: {}\n".format(load_avg))
                 f.write("^average number of processes waiting to be executed over the last 5 minutes\n")
             except AttributeError:
                 print("Load average not supported on this system.")
                 f.write("User Load Average: Not supported\n")
+            
             disk_usage = psutil.disk_usage('/').percent
-            print("Disk Space Consumed: {}%".format(disk_usage))  # Debugging print statement
+            print("Disk Space Consumed: {}%".format(disk_usage)) # Debugging print statement
             f.write("\nDisk Space Consumed: {}%\n".format(disk_usage))
             f.write("^percentage of disk space currently in use\n")
+            
             # Test response time
             server_ip = get_ip_address()
             average_time = test_response_time(server_ip)
