dbfb0e0b972ad1ed4567a1736fdd7ace26f9ed54
DiegoCon06 <147194771+DiegoCon06@users.noreply.github.com>
2025-04-30 00:55:55 -0500

    Update SUSS.py

--- SUSS.py
+++ SUSS.py
@@ -35,6 +35,7 @@ def generate_cpu_load(duration=60):
     """Generate CPU load by performing intensive calculations."""
     end_time = time.time() + duration
     while time.time() < end_time:
+        print(psutil.cpu_percent(interval=15))
         [x**2 for x in range(100000)]  # Increased range
         for _ in range(100):  # Added nested loop
             [x**3 for x in range(10000)]  # More complex calculations
